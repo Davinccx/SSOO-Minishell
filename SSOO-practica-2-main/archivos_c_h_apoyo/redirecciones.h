d87b99795d71f92aaf867af1f0f506aa6e3c201f
Davinccx <64002442+Davinccx@users.noreply.github.com>
2021-12-20 09:23:39 +0100

    Acabado

--- SSOO-practica-2-main/archivos_c_h_apoyo/redirecciones.h
+++
@@ -1,8 +0,0 @@
-#ifndef REDIRECCIONES_H
-#define REDIRECCIONES_H
-
-void redirec_entrada(char **args, int indice_entrada, int *entrada);
-void redirec_salida(char **args, int indice_salida, int *salida);
-
-
-#endif
